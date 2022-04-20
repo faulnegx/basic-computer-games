ad1a131cb88019f83d826739a26660dbf9fef862
axle <github.ducky@slmail.me>
2022-04-20 00:18:12 -0700

    My first rust toy function

---
+++ 03_Animal/rust/prompt.rs
@@ -0,0 +1,14 @@
+
+fn prompt(prompt_text: &str){
+   println!("{}", prompt_text);
+}
+
+
+fn main(){
+   let mut line = String::new();
+   println!("Enter your name :");
+   let b1 = std::io::stdin().read_line(&mut line).unwrap();
+   println!("Hello , {}", line);
+   println!("no of bytes read , {}", b1);
+   prompt("45");
+}
