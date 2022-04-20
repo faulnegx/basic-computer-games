3e818c3d769ee4cdab8cbe287be8878bc61c7e1e
axle <github.ducky@slmail.me>
2022-04-20 01:14:07 -0700

    Add reportAnimalsList and doesItSwim

--- 03_Animal/rust/prompt.rs
+++ 03_Animal/rust/prompt.rs
@@ -1,3 +1,5 @@
+#![allow(warnings)]
+
 // Prompt pass through here
 fn prompt(prompt_text: &str) -> String {
    println!("{}", prompt_text);
@@ -9,17 +11,35 @@ fn prompt(prompt_text: &str) -> String {
 fn start() {
    let response = prompt("ARE YOU THINKING OF AN ANIMAL(Y/N)?");
    if "Y" == response {
-      println!("This is a y");
+      doesItSwim();
+   } else if "LIST" == response {
+      reportAnimalsList();
+   } else {
+      start();
+   }
+}
+
+fn reportAnimalsList() {
+   // report list of animals known
+   let animalsList = ["BIRD", "FISH"];
+   println!("{:?}", animalsList);
+   start()
+}
+
+fn doesItSwim() {
+   let response = prompt("DOES IT SWIM(Y/N)?");
+   if "Y" == response {
+      println!("This animal swims.");
    } else {
       println!("this is not a why");
    }
 }
 
-
 fn main(){
    println!("PLAY 'GUESS THE ANIMAL'\n");
 
    println!("THINK OF AN ANIMAL AND THE COMPUTER WILL TRY TO GUESS IT.\n");
+   let mut xs = vec![1i32, 2, 3];
    start();
 
 
